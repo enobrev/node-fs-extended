d67ac390a8854b6bbaed6c8ca484800ddafb4daa
Mark Armendariz <src@enobrev.com>
2011-09-22 11:09:46 -0400

    Forgot to include the exec command

--- toolbox.js
+++ toolbox.js
@@ -1,6 +1,7 @@
     var fs   = require('fs');
     var path = require('path');
     var util = require('util');
+    var exec = require("child_process").exec
 
     exports.removeDirectory = function(sPath, fCallback) {
         fCallback = typeof fCallback == 'function' ? fCallback  : function() {};
