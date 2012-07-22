e920e6fbc82ff0c2256793493e9eb3cab3711f89
Mark Armendariz <src@enobrev.com>
2012-07-22 08:19:49 +0000

    catching error case in copyDirectoryPropertiesToFile.

--- fs-extended.js
+++ fs-extended.js
@@ -48,11 +48,15 @@
 
         var sPath = path.dirname(sFile);
         fs.stat(sPath, function(oError, oStat) {
-            fs.chmod(sFile, oStat.mode, function() {                                        // Copy Permissions from Parent Directory
-                fs.chown(sFile, oStat.gid, oStat.uid, function() {                          // Copy Ownership from Parent Directory
-                    fCallback(sFile);
+            if (oError) {
+                console.error('Error', oError);
+            } else {
+                fs.chmod(sFile, oStat.mode, function() {                                        // Copy Permissions from Parent Directory
+                    fs.chown(sFile, oStat.gid, oStat.uid, function() {                          // Copy Ownership from Parent Directory
+                        fCallback(sFile);
+                    });
                 });
-            });
+            }
         });
     };
 
